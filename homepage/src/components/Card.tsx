c8f41c0f68bcd4821421d7a2185e5f67b68a9f10
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-04-24 17:54:23 +0200

    contact form font

--- homepage/src/components/Card.tsx
+++ homepage/src/components/Card.tsx
@@ -2,25 +2,19 @@
 export default function Card() {
 	return (
 		<div >
-			<div className=" float-center
+			<div className="font-ubuntu font-semibold float-center text-xl
 							rounded-xl bg-white bg-clip-border 
-							text-gray-700 shadow-2xl">
+							text-gray-700 shadow-2xl antialiased">
 				<div className="p-6">
-					<h5 className="text-left
-								experience-task-style text-xl 
-								font-semibold text-blue-gray-900 
-								antialiased">
+					<h5 className="text-left">
 						Feel free to reach me at
 					</h5>
 					<ul>
-						<li className="m-5 text-xl ">
+						<li className="m-5 text-center">
 							<span><i className="fa-solid fa-envelope" ></i> </span>
 							<span>info@pocketgroovy.com</span>
 						</li>
-						<p className="text-left
-								experience-task-style text-xl 
-								font-semibold text-blue-gray-900 
-								">
+						<p className="text-left">
 						or use the contact form below
 					</p>
 					</ul>
--- homepage/src/components/ContactForm.tsx
+++ homepage/src/components/ContactForm.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, ReactElement, useState } from 'react';
 
 
+
 export default function ContactForm():ReactElement {
 	const [emailForm, setEmailForm] = useState({
 		subject: '',
@@ -8,103 +9,99 @@ export default function ContactForm():ReactElement {
 		email: '',
 		message: '',
 	});
-
+	
 	//Result if message was sent or not
 	const [result, setResult] = useState('');
-
+	
 	//Status of while message is being sent
 	const [status, setStatus] = useState('Submit');
 
+const resetEmailForm = () => {
+	setEmailForm({ subject: '', username: '', email: '', message: '' });
+}
 
-	function resetEmailForm() {
-		setEmailForm({ subject: '', username: '', email: '', message: '' });
-	}
+const handleEmailFormChange = (event: ChangeEvent<HTMLInputElement> ) => {
+	event.persist();
+	const target = event.target;
+const name = target.name;
+	setEmailForm((prevEmailData) => {
+		return {
+			...prevEmailData,
+			[name]: target.value
+		};
+	});
 
-	const handleEmailFormChange = (event: ChangeEvent<HTMLInputElement> ) => {
-		event.persist();
-		const target = event.target;
-    const name = target.name;
-		setEmailForm((prevEmailData) => {
-			return {
-				...prevEmailData,
-				[name]: target.value
-			};
-		});
-
-		if (result.length > 0) {
-			setResult('');
-		}
-	}
-
-	const handleEmailMessageChange = (event: ChangeEvent<HTMLTextAreaElement> ) => {
-		event.persist();
-		const target = event.target;
-		const name = target.name;
-		setEmailForm((prevEmailData) => {
-			return {
-				...prevEmailData,
-				[name]: target.value
-			};
-		});
-
-		if (result.length > 0) {
-			setResult('');
-		}
-	}
-
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+	if (result.length > 0) {
 		setResult('');
-		e.preventDefault();
-		setStatus('Sending...');
-		const target = e.currentTarget;
+	}
+}
 
-		const details = { 
-			subject: target.subject.value,
-			username: target.username.value,
-			email: target.email.value,
-			message: target.message.value
-		 };
+const handleEmailMessageChange = (event: ChangeEvent<HTMLTextAreaElement> ) => {
+	event.persist();
+	const target = event.target;
+	const name = target.name;
+	setEmailForm((prevEmailData) => {
+		return {
+			...prevEmailData,
+			[name]: target.value
+		};
+	});
 
-	
+	if (result.length > 0) {
+		setResult('');
+	}
+}
 
-		console.log(details);
+const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+	setResult('');
+	e.preventDefault();
+	setStatus('Sending...');
+	const target = e.currentTarget;
 
-		try {
-			let response = await fetch('http://localhost:5000/send', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json;charset=utf-8',
-				},
-				body: JSON.stringify(details),
-			});
-			setStatus('Submit');
-			let result = await response.json();
+	const details = { 
+		subject: target.subject.value,
+		username: target.username.value,
+		email: target.email.value,
+		message: target.message.value
+	 };
 
-			if (result.status === 'success') {
-				setResult('Message Sent!');
-				resetEmailForm();
-			} else if (result.status === 'fail') {
-				alert('Uh oh! Message failed to send.');
-			}
-		} catch (error) {
-			console.error(error);
-			setStatus('Submit');
-			setResult('Sorry! We are experiencing some issues with submitting message.');
+	console.log(details);
+
+	try {
+		let response = await fetch('http://localhost:5000/send', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+			},
+			body: JSON.stringify(details),
+		});
+		setStatus('Submit');
+		let result = await response.json();
+
+		if (result.status === 'success') {
+			setResult('Message Sent!');
+			resetEmailForm();
+		} else if (result.status === 'fail') {
+			alert('Uh oh! Message failed to send.');
 		}
-	};
-
+	} catch (error) {
+		console.error(error);
+		setStatus('Submit');
+		setResult('Sorry! We are experiencing some issues with submitting message.');
+	}
+};
 	return (
-		<div className="p-1 experience-task-style float-center
+		<div className="p-1 font-ubuntu float-center
 						rounded-xl shadow-2xl bg-white bg-clip-border  ">
 			<form id='contact-form' className='form'
 				onSubmit={handleSubmit}
 				method='POST'>
 				<label className="text-2xl"> Contact Form
 				<div>
-					<label className="field text-sm">Select Category
+					<label className="text-sm">Select Category
 						<br></br>
-						<select name='subject' className="contact-input">
-							<option value="None" >
+						<select name='subject' className="contact-input" required>
+							<option value="">
 								-- Select Your Query --
 							</option>
 							<option value="General Question" >
@@ -119,8 +116,8 @@ export default function ContactForm():ReactElement {
 						</select>
 					</label>
 					<br></br>
-					<div className='field'>
-					<label  className="text-sm">Name*
+					<div>
+					<label  className="text-sm">Name
 						<input name='username' className="contact-input"
 							type="name"
 							placeholder="your name"
@@ -131,7 +128,7 @@ export default function ContactForm():ReactElement {
 					</label>
 					</div>
 					<label  className="text-sm">
-						Email Address*
+						Email Address
 						<input name='email' className="contact-input"
 							type="email"
 							placeholder="your@email.com"
