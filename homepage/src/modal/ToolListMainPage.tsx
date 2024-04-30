3cd7d6126194ad7e28972df16c51fd876c7354d3
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-04-30 14:42:46 +0200

    add stuck

--- homepage/src/modal/ToolListMainPage.tsx
+++ homepage/src/modal/ToolListMainPage.tsx
@@ -3,11 +3,13 @@ import { ReactElement } from "react";
 export default function ToolListMainPage(): ReactElement{
     return (
         <ul className="list-outside list-disc text-lg">
-            <li>React <span className="text-sm" >:UI library</span></li>
-            <li>TypeScript <span className="text-sm" >:language</span></li>
-            <li>TailwindCSS <span className="text-sm" >:css framework</span></li>
-            <li>Vite <span className="text-sm" >:build tool</span></li>
-            <li>Nginx <span className="text-sm" >:reverse proxy</span></li>
+            <li>react <span className="text-sm" >:UI library</span></li>
+            <li>typeScript <span className="text-sm" >:language</span></li>
+            <li>tailwindCSS <span className="text-sm" >:css framework</span></li>
+            <li>vite <span className="text-sm" >:build tool</span></li>
+            <li>node.js <span className="text-sm" >:backend framework</span></li>
+            <li>express.js <span className="text-sm" >:web app framework</span></li>
+            <li>nginx <span className="text-sm" >:reverse proxy</span></li>
             <li>VS Code <span className="text-sm" >:code editor</span></li>
         </ul>
     )
