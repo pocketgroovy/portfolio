4310f4838abec962d0ef22a1c4ce2034c3a29d51
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-05-02 15:50:33 +0200

    revert

--- server/logger.js
+++ server/logger.js
@@ -1,8 +1,8 @@
 import winston from "winston";
-const { combine, timestamp, json, errors, createLogger, transports} = winston.format;
+const { combine, timestamp, json, errors } = winston.format;
 
 
-const logger = createLogger({
+const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   // Use timestamp and printf to create a standard log format
   format: combine(
@@ -13,8 +13,8 @@ const logger = createLogger({
     )
   ),
   transports: [
-    new transports.Console(),
-    new transports.File({ filename: "logs/email.log" }),
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/email.log" }),
   ],
 });
 
