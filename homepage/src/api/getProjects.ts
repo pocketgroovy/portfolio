91ee8ae1a2511b5883c02827a3b480a02477ef5d
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-05-08 11:48:20 +0200

    dbserver

--- homepage/src/api/getProjects.ts
+++ homepage/src/api/getProjects.ts
@@ -1,8 +1,8 @@
 import type { IProject } from '../types/iproject'
 import axios, {AxiosResponse} from 'axios'
 
-// const BASE_URL: string = "https://dbserver.pocketgroovy.com"
-const BASE_URL: string = "https://localhost:3660"
+const BASE_URL: string = "https://dbserver.pocketgroovy.com"
+// const BASE_URL: string = "https://localhost:3660"
 
 const apiService = axios.create({
     baseURL: BASE_URL
