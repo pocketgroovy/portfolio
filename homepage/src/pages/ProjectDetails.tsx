b60557766caeb93a302efa19c55302178bc882aa
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-05-17 11:20:55 +0200

    mern image

--- homepage/src/pages/ProjectDetails.tsx
+++ homepage/src/pages/ProjectDetails.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
 import getProjects from 'api/getProjects'
 import AttributionTag from 'components/AttributionTag'
+import ImagePane from 'components/ImagePane'
 // import Head from 'components/Head'
 import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
 import { Link, Navigate, useParams } from 'react-router-dom'
 import type { IAttribute } from 'types/iattribute'
 import { useMediaQuery } from 'utils'
+import image from '../media/images/mern.png'
+
 
 const DESKTOP_IMAGE_WIDTH_PERCENTAGE = 0.4
 const MOBILE_IMAGE_HEIGHT_PERCENTAGE = 0.3
@@ -119,14 +122,16 @@ export default function ProjectDetails(): ReactElement {
                     <h1 className='font-bold underline decoration-from-font mt-5 my-5 text-3xl'>
                         Behind the Scenes: Insights into Development
                     </h1>
+
                     <div className='ps-px pr-64 pb-10 font-serif4 text-xl  text-justify tracking-wide space-y-5'>
                         <p>The architecture for these portfolio webpages is based on the 3 tiers: Presentation, Application, and Data tiers. The main tools used were the MERN stack, comprising MongoDB, Express, React, and Node.</p>
                         <p>I chose this stack primarily for its popularity, which makes it easier to obtain the necessary information.</p>
                         <p>As for the infrastructure setup, I first configured the Hostinger website to host the presentation pages, which users will interact with. Then, I set up a VPS (Virtual Private Server) to host email and database servers using Express.</p>
                         <p>For enhanced security, I utilized Nginx to reverse-proxy requests to the servers. Speaking of security, SSL was essential for me. Hostinger Web Hosting provides free SSL certificates, eliminating the need for manual setup. All I had to do was associate my domain with the web hosting, and SSL was installed automatically.</p>
                         <p>The email and database servers, residing in the VPS, were assigned to subdomains. Although SSL installation was not automatic for these, Hostinger provided documentation (<a className='text-blue-600 visited:text-purple-600 ...' href="https://support.hostinger.com/en/articles/6865487-how-to-install-ssl-on-vps-using-certbot">https://support.hostinger.com/en/articles/6865487-how-to-install-ssl-on-vps-using-certbot</a>), making the setup relatively straightforward.</p>
-                        <p>Having longer career as a backend engineer, most of the frontend tools used here were new to me. Learning and trial and error took some time. I've spent about 4-5 weeks to get to where you see now, including all the infrastructure setups.</p>
-                        <p>Please feel free to contat me for any information!</p>
+                        <ImagePane imagePath={image} imageTitle='mern' />
+                        <p> Please feel free to contat me!</p>
+
                         <Link className='font-bold' to={'/contacts'}>
                             <span className="flex"><i className="mr-2 fa-regular fa-message flex items-start" />CONTACTS</span>
                         </Link>
